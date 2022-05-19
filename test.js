85f90242f90b5475666b911da56f42c75836e7e1
Vitche Research Team Developer <developer@vitche.com>
2022-05-20 00:16:37 +0300

    1. The method to get the events listing.

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 import Reference from "./main.js";
 
 const reference = new Reference("http://34.239.11.167:85");
-const eventsPromise = reference.event().list("62379ef264bd68001d0a581a");
+const eventsPromise = reference.event().list("6232623f64bd68001d0a2ac1");
 eventsPromise.then((results) => {
     console.log(results);
 });
